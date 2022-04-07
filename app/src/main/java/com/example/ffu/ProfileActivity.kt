69c897a1eeff020d6e09c1b9e5f26c33060846e8
YongHyunKing <99469068+YongHyunKing@users.noreply.github.com>
2022-04-07 18:43:56 +0900

    4/7 1

--- app/src/main/java/com/example/ffu/ProfileActivity.kt
+++ app/src/main/java/com/example/ffu/ProfileActivity.kt
@@ -10,6 +10,19 @@ class ProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.profile)
 
+        val home = findViewById<Button>(R.id.profile_home)
+        val editprofile_button = findViewById<Button>(R.id.profile_profileedit_button)
+
+
+        home.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        editprofile_button.setOnClickListener {
+            val intent = Intent(this, ProfileSettingActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 
