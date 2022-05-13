c90da3bd37184a95f0266c0c288ab0e1c722edc5
SeungGyuu <seunggyu.dev@gmail.com>
2022-05-14 03:01:30 +0900

    google-services.json update

--- app/src/main/java/com/example/ffu/RootActivity.kt
+++ app/src/main/java/com/example/ffu/RootActivity.kt
@@ -1,18 +1,25 @@
 package com.example.ffu
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
+
 
 class RootActivity : AppCompatActivity() {
 
-    private val auth : FirebaseAuth = FirebaseAuth.getInstance()
-
+    //private val auth : FirebaseAuth = FirebaseAuth.getInstance()
+    var PICK_IMAGE_FROM_ALBUM = 0
+    var storage : FirebaseStorage? = null
+    var photoUri : Uri? = null
+    var resultLauncher: ActivityResultLauncher<Intent>? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.root)
-    }
 
     override fun onStart() {
         super.onStart()
