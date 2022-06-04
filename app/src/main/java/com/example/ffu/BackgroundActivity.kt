ed56ea12f07327e490bf87e5f3ca551f58906dfb
moungjae joe <cmj092222@naver.com>
2022-06-04 23:48:14 +0900

    2022-06-04 : merge

--- app/src/main/java/com/example/ffu/BackgroundActivity.kt
+++ app/src/main/java/com/example/ffu/BackgroundActivity.kt
@@ -1,6 +1,7 @@
 package com.example.ffu
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
@@ -33,6 +34,11 @@ class BackgroundActivity : AppCompatActivity() {
 
     private lateinit var binding: BackgroundBinding
     private lateinit var userDB: DatabaseReference
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this, "종료!!!", Toast.LENGTH_SHORT).show()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
