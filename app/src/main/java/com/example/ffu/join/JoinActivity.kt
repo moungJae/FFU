7c16a8f831ed0c6d16b26f597d356d28163c8d63
SeungGyuu <seunggyu.dev@gmail.com>
2022-06-06 22:19:14 +0900

    06-06: Join UI update, menu scroll update

--- app/src/main/java/com/example/ffu/join/JoinActivity.kt
+++ app/src/main/java/com/example/ffu/join/JoinActivity.kt
@@ -1,16 +1,12 @@
 package com.example.ffu.join
 
 import android.content.Intent
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.view.LayoutInflater
+import android.util.Log
 import android.view.View
-import android.view.Window
 import android.widget.*
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.ffu.R
 import com.example.ffu.profile.ProfileSettingActivity
@@ -18,12 +14,12 @@ import com.metagalactic.dotprogressbar.DotProgressBar
 
 class JoinActivity : AppCompatActivity() {
 
-    private lateinit var birthText : TextView
-    private lateinit var progressBar : DotProgressBar
+    private lateinit var birthText: TextView
+    private lateinit var progressBar: DotProgressBar
 
-    private var birth : String = ""
-    private var gender : String = ""
-    private var myYear = 1970
+    private var birth: String = ""
+    private var gender: String = ""
+    private var myYear = 2000
     private var myMonth = 1
     private var myDay = 1
 
@@ -38,76 +34,53 @@ class JoinActivity : AppCompatActivity() {
 
     private fun initialSetting() {
         progressBar = findViewById<DotProgressBar>(R.id.join_progressbar)
-        birthText = findViewById<TextView>(R.id.join_text_birth)
     }
 
     private fun checkBirth() {
-        val birthButton = findViewById<Button>(R.id.join_yearMonth_button)
+        val year = findViewById<NumberPicker>(R.id.dialog_datePicker_year)
+        val month = findViewById<NumberPicker>(R.id.dialog_datePicker_month)
+        val day = findViewById<NumberPicker>(R.id.dialog_datePicker_day)
 
-        birthButton.setOnClickListener {
-            val dialog = AlertDialog.Builder(this).create()
-            val edialog : LayoutInflater = LayoutInflater.from(this)
-            val mView : View = edialog.inflate(R.layout.dialog_datepicker,null)
-            val year : NumberPicker = mView.findViewById(R.id.dialog_datePicker_year)
-            val month : NumberPicker = mView.findViewById(R.id.dialog_datePicker_month)
-            val day : NumberPicker = mView.findViewById(R.id.dialog_datePicker_day)
-            val cancel : Button = mView.findViewById(R.id.dialog_datePicker_cancel)
-            val save : Button = mView.findViewById(R.id.dialog_datePicker_save)
+        year.wrapSelectorWheel = false
+        month.wrapSelectorWheel = false
+        day.wrapSelectorWheel = false
+        year.minValue = 1970
+        month.minValue = 1
+        day.minValue = 1
 
-            //  순환 안되게 막기
-            year.wrapSelectorWheel = false
-            month.wrapSelectorWheel = false
-            day.wrapSelectorWheel = false
+        //  최대값 설정, 월마다 최대 일을 지정해야하는디..윤년
+        year.maxValue = 2015
+        month.maxValue = 12
+        day.maxValue = 31
+        // 현재 지정값 설정
+        year.value = myYear
+        month.value = myMonth
+        day.value = myDay
 
-            //  editText 설정 해제
-            year.descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
-            month.descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
-            day.descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
-
-            //  최소값 설정
-            year.minValue = 1970
-            month.minValue = 1
-            day.minValue = 1
-
-            //  최대값 설정
-            year.maxValue = 2021
-            month.maxValue = 12
-            day.maxValue = 31
-
-            // 현재 지정값 설정
-            year.value = myYear
-            month.value = myMonth
-            day.value = myDay
-
-            //  취소 버튼 클릭 시
-            cancel.setOnClickListener {
-                dialog.dismiss()
-                dialog.cancel()
+        year.descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
+        month.descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
+        day.descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
+        val listener = NumberPicker.OnValueChangeListener { numberPicker, old, new ->
+            when (numberPicker) {
+                year -> myYear = new
+                month -> myMonth = new
+                day -> myDay = new
+                else -> Log.d("birth", "$year $month $day")
             }
-            //  완료 버튼 클릭 시
-            save.setOnClickListener {
-                birth = (year.value).toString() + "/" + (month.value).toString() + "/" + (day.value).toString()
-                birthText.setText(birth)
-                myYear = year.value
-                myMonth = month.value
-                myDay = day.value
-                dialog.dismiss()
-                dialog.cancel()
-            }
-            dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
-            dialog.setView(mView)
-            dialog.create()
-            dialog.show()
         }
+        year.setOnValueChangedListener(listener)
+        month.setOnValueChangedListener(listener)
+        day.setOnValueChangedListener(listener)
     }
 
     private fun checkGender() {
-        val genderRadio = findViewById<RadioGroup>(R.id.join_genderRadio)
+        val checkGender = findViewById<RadioGroup>(R.id.checkGender)
 
-        when (genderRadio.checkedRadioButtonId) {
-            R.id.join_female -> gender = "여자"
-            R.id.join_male -> gender = "남자"
+        checkGender.setOnCheckedChangeListener { radioGroup, i ->
+            when (i) {
+                R.id.checkFemale -> gender = "여자"
+                R.id.checkMale -> gender = "남자"
+            }
         }
     }
 
@@ -122,16 +95,19 @@ class JoinActivity : AppCompatActivity() {
 
     private fun checkData() {
         val nextButton = findViewById<Button>(R.id.join_joinNext)
-
         nextButton.setOnClickListener {
             checkGender()
-            if (birth.isEmpty() || gender.isEmpty()) {
-                Toast.makeText(this, "정보를 입력해주세요.", Toast.LENGTH_SHORT).show()
-            }
-            else {
+            birth = (myYear).toString() + "/" + (myMonth).toString() + "/" + (myDay).toString()
+            Log.d("birth", birth)
+            Log.d("gender", gender)
+            if (birth.isEmpty()) {
+                Toast.makeText(this, "생일을 선택해주세요.", Toast.LENGTH_SHORT).show()
+            } else if (gender.isEmpty()) {
+                Toast.makeText(this, "성별을 선택해주세요.", Toast.LENGTH_SHORT).show()
+            } else {
                 progressBar.visibility = View.VISIBLE
                 Thread(Runnable {
-                    Thread.sleep(1500)
+                    Thread.sleep(500)
                     Handler(Looper.getMainLooper()).post {
                         moveNext()
                     }
